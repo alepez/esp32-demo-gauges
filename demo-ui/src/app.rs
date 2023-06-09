6b431e3dee50113676c26379f9bfb49fdfd1f279
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-06-09 18:58:24 +0200

    Add gauge

--- demo-ui/src/app.rs
+++ demo-ui/src/app.rs
@@ -1,7 +1,11 @@
+use angle::Rad;
 use demo::app::SystemState;
 use dioxus::prelude::*;
 use dioxus_websocket_hooks::use_ws_context_provider_json;
 use fermi::{use_init_atom_root, use_read, use_set, Atom};
+use gauges::app::gauge::Gauge;
+use gauges::prelude::*;
+use std::f64::consts::PI;
 use std::rc::Rc;
 
 pub static SYSTEM_STATE: Atom<Option<SystemState>> = |_| None;
@@ -20,11 +24,37 @@ pub fn App(cx: Scope) -> Element {
     cx.render(rsx!(Main {},))
 }
 
+const ARC_STYLE: GaugeStyle = GaugeStyle::Arc(ArcGaugeStyle {
+    radius: 50.0,
+    begin_angle: Rad(225.0 / 180.0 * PI),
+    full_width: Rad(270.0 / 180.0 * PI),
+});
+
 #[allow(non_snake_case)]
 fn Main(cx: Scope) -> Element {
     if let Some(system_state) = use_read(cx, SYSTEM_STATE) {
-        let text = format!("{}", system_state.counter);
-        cx.render(rsx!(div { text }))
+        let value = Value::Float(system_state.counter as f64);
+        let signal = SignalInfo {
+            name: Some("Counter".to_owned()),
+        };
+        let style = ARC_STYLE;
+        let range = Range {
+            min: 0.,
+            max: 1_000_000.,
+        };
+        let format = GaugeTextFormat { precision: 0 };
+        let age = Age::New;
+
+        cx.render(rsx! {
+            Gauge {
+                value: value,
+                signal: signal,
+                style: style,
+                range: range,
+                format: format,
+                age: age,
+            }
+        })
     } else {
         cx.render(rsx!(div { "loading..." }))
     }
