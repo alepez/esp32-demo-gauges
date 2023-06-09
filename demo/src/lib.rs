547b97f4150029e9d6328d053dc2e4a4754e39a8
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-06-09 16:32:01 +0200

    WIP Clean up

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,20 +1,14 @@
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Vec3 {
     x: f32,
     y: f32,
     z: f32,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct SystemState {
     acc: Vec3,
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
